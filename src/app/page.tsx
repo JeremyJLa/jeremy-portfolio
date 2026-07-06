78d60c525799160936473c10be674d8c71b01dde
Jeremy Laycock <YOUR-GITHUB-EMAIL@example.com>
2026-07-06 17:55:04 +1000

    Change heading color to red

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-4xl font-semibold tracking-tight text-black dark:text-zinc-50">
+      <h1 className="text-4xl font-semibold tracking-tight text-red-600">
         My Portfolio
       </h1>
     </div>
